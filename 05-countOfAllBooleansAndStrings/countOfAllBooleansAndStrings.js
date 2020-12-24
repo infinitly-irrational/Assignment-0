ae26a0b5aeb03e5b15da54e8aa619577d0b50f26
Haitham Alnajar <Haitham.alnajar98@gmail.com>
2020-12-24 02:29:03 -0500

    Problem 05 solution

---
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -0,0 +1,13 @@
+function countOfAllBooleansAndStrings(arr) {
+
+  let x=0;
+
+  for (let i=0; i < arr.length; i++){
+      if (typeof arr[i] == "boolean" || typeof arr[i] == "string")
+            x++;
+  }
+  return x;
+}
+
+// Do not edit this line;
+module.exports = countOfAllBooleansAndStrings;
