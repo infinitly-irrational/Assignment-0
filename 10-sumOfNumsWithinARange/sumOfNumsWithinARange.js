cfb9e28a834a98c01bac796ddd2cab666c5b348c
Haitham Alnajar <Haitham.alnajar98@gmail.com>
2020-12-24 16:37:46 -0500

    Problem 10 solution

---
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -0,0 +1,12 @@
+function cd(nums, start, end) {
+  let x=0;
+
+  for (let i=0; i < nums.length; i++){
+      if (nums[i] >= start && nums[i] <= end)
+          x++;
+  }
+    return x;
+}
+
+// Do not edit this line;
+module.exports = sumOfNumsWithinARange;
